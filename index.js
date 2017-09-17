b50b5eff1a20052d7e4da1e9722a8b13d2fedbf5
BernhardBerbuir <BernhardBerbuir@users.noreply.github.com>
2017-09-17 23:04:25 +0200

    add eludium and thorium, use seconds for timer

--- index.js
+++ index.js
@@ -137,14 +137,14 @@
     }
 
     class PeriodicTask extends AsyncTask {
-        constructor(settings, intervalInMinutes = 4) {
-            super(Object.assign({ interval: intervalInMinutes * 60 }, settings));
+        constructor(settings, intervalInSeconds = 300) {
+            super(Object.assign({ interval: intervalInSeconds }, settings));
             this.secondsSinceLastExecution = 0;
 
             // Generate toggle
             this.$toggle.find('.task-label').on('click', () => this.$toggle.toggleClass('show-incrementer'));
-            const $incrementer = $('<input class="task-interval" type="number" max="60" min="1" step="1" />');
-            $incrementer.val(intervalInMinutes);
+            const $incrementer = $('<input class="task-interval" type="number" max="3600" min="5" step="5" />');
+            $incrementer.val(intervalInSeconds);
             $incrementer.on('change', event => this.setInterval(event.currentTarget.value));
             this.$toggle.prepend($incrementer);
             this.$toggle.append('<small class="task-timer" />');
@@ -360,11 +360,13 @@
         { name: 'plate', icon: '🔗' },
         { name: 'steel', icon: '⚔️' },
         { name: 'alloy', icon: '🔩' },
+        { name: 'eludium', icon: 'E' },
         { name: 'kerosene', icon: '🛢' },
         { name: 'parchment', icon: '📝' },
         { name: 'manuscript', icon: '🗞' },
         { name: 'compedium', label: 'compendium', icon: '📖' },
-        { name: 'blueprint', icon: '📘' }
+        { name: 'blueprint', icon: '📘' },
+        { name: 'thorium', icon: 'T' },
     ].map(a => new Autocrafter(a.icon, a.name, a.label));
 
     tasks.push(new Autohunter('🐯', 'hunt'));
